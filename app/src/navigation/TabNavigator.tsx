b8de0e9356115a094307dc059304245548a028c9
David Beno <dawid.beno@gmail.com>
2026-02-21 20:44:17 +0100

    tab bar icons

--- app/src/navigation/TabNavigator.tsx
+++ app/src/navigation/TabNavigator.tsx
@@ -5,14 +5,50 @@ import ActivityScreen from '../screens/ActivityScreen';
 import FeedScreen from '../screens/FeedScreen';
 import SearchScreen from '../screens/SearchScreen';
 import NewPostScreen from '../screens/NewPostScreen';
+import Ionicons from '@expo/vector-icons/build/Ionicons';
 
 const TabNavigator = createBottomTabNavigator({
     screens: {
-        Home: FeedScreen,
-        Search: SearchScreen,
-        NewPost: NewPostScreen,
-        Activity: ActivityScreen,
-        Profile: ProfileScreen,
+        Home: {
+            screen: FeedScreen,
+            options: {
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="home-outline" size={size} color={color} />
+                ),
+            }
+        },
+        Search: {
+            screen: SearchScreen,
+            options: {
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="search-outline" size={size} color={color} />
+                ),
+            }
+        },
+        NewPost: {
+            screen: NewPostScreen,
+            options: {
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="add-circle-outline" size={size} color={color} />
+                ),
+            }
+        },
+        Activity: {
+            screen: ActivityScreen,
+            options: {
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="heart-outline" size={size} color={color} />
+                ),
+            }
+        },
+        Profile: {
+            screen: ProfileScreen,
+            options: {
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="person-outline" size={size} color={color} />
+                ),
+            }
+        },
     }
 });
 

deeddc3243fc17a46fe40d92e475b0986e0bfb4d
David Beno <dawid.beno@gmail.com>
2026-02-28 10:59:40 +0100

    filter tabs component

---
+++ app/src/components/FilterTabs.tsx
@@ -0,0 +1,50 @@
+import { View, TouchableOpacity, Text, StyleSheet, ScrollView } from "react-native";
+import { useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function FilterTabs() {
+    const [activeFilter, setActiveFilter] = useState("All");
+    const filters = ["All", "Follows", "Replies", "Mentions", "Quotes"]
+    return (
+        <LinearGradient
+            colors={['#fff', '#fff', 'rgba(255,255,255,0)']}
+            locations={[0, 0.25, 1]}
+            style={styles.scrollViewBackground}
+            >
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            {filters.map((filter) => {
+                const isActive = filter === activeFilter
+                return (
+                    <TouchableOpacity key={filter} style={[styles.pillBorder, isActive && styles.activePillBorder]} onPress={() => setActiveFilter(filter)}>
+                        <Text style={styles.pillText}>{filter}</Text>
+                    </TouchableOpacity>
+                )
+            })}
+            </ScrollView>
+        </LinearGradient>
+    );
+}
+
+const styles = StyleSheet.create({
+    scrollViewBackground: {
+        marginBottom: -10,
+    },
+    pillBorder: {
+        borderWidth: 0.5,
+        borderColor: '#aaa',
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        paddingHorizontal: 14,
+        paddingVertical: 8,
+        marginLeft: 8,
+    },
+    activePillBorder: {
+        borderColor: '#000',
+        backgroundColor: '#eee'
+    },
+    pillText: {
+        fontSize: 16,
+        fontWeight: '500',
+    },
+});
+
